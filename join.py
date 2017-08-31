c19a3678ff166822e1e0074db1680c22725f75ed
SM Abu Taher Asif <shayokhbd144@gmail.com>
2017-08-31 11:54:36 +0600

    Add files via upload

--- join.py
+++ join.py
@@ -13,13 +13,18 @@ import pandas as pd
 
 
 def joinOriginAndDestination(FlightDelaysData , AirportCodesDataset):
-    
-    df=pd.merge(FlightDelaysData, AirportCodesDataset, how = 'left',left_on ='OriginAirportID',right_on ='airport_id')# join on 	OriginAirportID	,airport_id
+    # join on 	OriginAirportID	,airport_id
+    df=pd.merge(FlightDelaysData, AirportCodesDataset, how = 'left',left_on ='OriginAirportID',right_on ='airport_id')
     df=df.drop(['airport_id'],axis=1)
     df=df.rename(columns={'city': 'origin_city', 'state': 'origin_state','name':'origin_name'})
+    # join on 	OriginAirportID	,DestAirportID
     df_destination=pd.merge(df, AirportCodesDataset, how = 'left',left_on ='DestAirportID',right_on ='airport_id')
     df_destination=df_destination.drop(['airport_id'],axis=1)
     df_final=df_destination.rename(columns={'city': 'dest_city', 'state': 'dest_state','name':'dest_name'})
+    #print number of times eacd reposts appeae
+    print(df_final.groupby(['dest_name']).size())
+    # most frequently appeared
+    print(df_final.groupby(['dest_name']).size().nlargest(1))
     return df_final
 
 def azureml_main(FlightDelaysData, AirportCodesDataset):
