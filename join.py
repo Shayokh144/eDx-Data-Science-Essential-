100ba0fb0ceb22f2c19e7a257d5c927814d85233
SM Abu Taher Asif <shayokhbd144@gmail.com>
2017-09-10 11:03:48 +0600

    Update join.py

--- join.py
+++ join.py
@@ -17,7 +17,7 @@ def joinOriginAndDestination(FlightDelaysData , AirportCodesDataset):
     df=pd.merge(FlightDelaysData, AirportCodesDataset, how = 'left',left_on ='OriginAirportID',right_on ='airport_id')
     df=df.drop(['airport_id'],axis=1)
     df=df.rename(columns={'city': 'origin_city', 'state': 'origin_state','name':'origin_name'})
-    # join on 	OriginAirportID	,DestAirportID
+    # join on 	DestAirportID, airport_id
     df_destination=pd.merge(df, AirportCodesDataset, how = 'left',left_on ='DestAirportID',right_on ='airport_id')
     df_destination=df_destination.drop(['airport_id'],axis=1)
     df_final=df_destination.rename(columns={'city': 'dest_city', 'state': 'dest_state','name':'dest_name'})
